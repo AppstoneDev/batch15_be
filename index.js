a773e5cdabce4f44928b4c64751682c3de154f9e
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-06-09 19:48:21 +0530

    POST method essentials added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 
 app.set("port", 8000);
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
 
 app.get("/", (req, res)=>{
   console.log(req.query.username);
@@ -12,7 +14,14 @@ app.listen(app.get("port"), () => {
   console.log("Express app is running on port " + app.get("port"));
 })
 
+app.post("/addDetail", (req, res)=>{
+  console.log(req.body);
+  res.send("Post method called successfully");
+})
+
 //ASSIGNMENT 1
 //CREATE A LOGIN VALIDATION API IN GET METHOD
 //SEND USERNAME AND PASSWORD
 //VALIDATE IT USING IF ELSE STATEMENT AND SEND A RESPONSE ACCORDINGLY. 
+
+
