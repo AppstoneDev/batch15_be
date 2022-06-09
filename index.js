7ce9f259828a9c100871ae8fcfb72c377623dfb8
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-06-09 19:58:37 +0530

    Assignment task added

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 
 app.set("port", 8000);
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   console.log(req.query.username);
   res.send("hello from default path");
 })
@@ -14,7 +14,7 @@ app.listen(app.get("port"), () => {
   console.log("Express app is running on port " + app.get("port"));
 })
 
-app.post("/addDetail", (req, res)=>{
+app.post("/addDetail", (req, res) => {
   console.log(req.body);
   res.send("Post method called successfully");
 })
@@ -25,3 +25,41 @@ app.post("/addDetail", (req, res)=>{
 //VALIDATE IT USING IF ELSE STATEMENT AND SEND A RESPONSE ACCORDINGLY. 
 
 
+//ASSIGNMENT2
+//STORE USER CREDENTIALS IN A ARRAY
+//USE A API TO SIGNUP AND VALIATE AND SECOND API TO LOGIN RETURN THE USER DETAILS
+
+
+
+
+
+const users = [];
+
+app.post("/signup", (req, res) => {
+  var username = req.body.username
+  var password = req.body.password
+  var email = req.body.email
+  var name = req.body.name
+  
+  console.log("api called");
+
+  if(users.length > 0){
+    for (var user of users){
+      if(user.username == username){
+        res.json({status:false, message:"username exists" });
+        return;
+      }
+    }
+  }
+
+    var newUser = {
+      "username" : username,
+      "password" : password,
+      "email" :email,
+      "name": name
+    }
+
+    users.push(newUser);
+    res.json({status:true, message:"User added successfully"});
+  
+})
